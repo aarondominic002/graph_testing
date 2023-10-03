df1680dfe831919b96823e2294c260555be2ea2b
aarondominic002 <ec22622@qmul.ac.uk>
2023-10-03 10:56:22 +0100

    test 3

--- Minimal_test.py
+++ Minimal_test.py
@@ -1,2 +1,17 @@
-print(222)
+from select import select
+import unittest
+from unittest import result
 
+# The function to be tested 
+
+def add_numbers(a, b):
+    return a + b 
+
+class TestFunction(unittest.TestCase):
+    def test_addition(self):
+        result = add_numbers(3, 5)
+        self.assertEqual(result, 8)
+    
+    def test_addition_negative_numbers(self):
+        result = add_numbers(-2, 7)
+        
