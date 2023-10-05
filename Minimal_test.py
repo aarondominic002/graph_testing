4a1f33b74878e6bbc0892fc287c2669a6925eacf
aarondominic002 <ec22622@qmul.ac.uk>
2023-10-05 09:44:16 +0100

    final

--- Minimal_test.py
+++ Minimal_test.py
@@ -1,22 +1,23 @@
 from select import select
 import unittest
 from unittest import result
+import function
 
 # The function to be tested 
-
+import function
 
 
 class TestFunction(unittest.TestCase):
     def test_addition(self):
-        result = add_numbers(3, 5)
+        result = function.add_numbers(3, 5)
         self.assertEqual(result, 8)
     
     def test_addition_negative_numbers(self):
-        result = add_numbers(-2, 6)
+        result = function.add_numbers(-2, 6)
         self.assertEqual(result,4)
 
     def test_addition_float_numbers(self):
-        result = add_numbers(1.5, 3.5)
+        result = function.add_numbers(1.5, 3.5)
         self.assertAlmostEqual(result, 5.0, places = 2 )
 
 if __name__ == "__main__":
